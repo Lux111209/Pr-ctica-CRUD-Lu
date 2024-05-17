ea14f0622fac3e0a4dad63b08cc5642275ecb0f3
Luz Gaspario <20200448@ricaldone.edu.sv>
2024-05-17 15:47:05 -0600

    Cree CRUD

---
+++ Luz_CRUD_Artistas.sql
@@ -0,0 +1,9 @@
+create table Artista (
+idArtista number,
+nombreArtista varchar2(100),
+nacionalidadArtista varchar2(50),
+edadArtista number,
+nombreArtisticoArtista varchar2(100)
+)
+
+select * from Artista
